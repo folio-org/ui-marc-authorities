86e40f5faf183d833ccbb19ccd3acf2ee3c2f772
Denys Bohdan <Denys_Bohdan@epam.com>
2021-04-26 17:49:49 +0300

    UIMARCAUTH-2 removed unneeded dependencies

--- test/jest/__mock__/stripesComponents.mock.js
+++ test/jest/__mock__/stripesComponents.mock.js
@@ -1,10 +1,3 @@
 jest.mock('@folio/stripes/components', () => ({
   ...jest.requireActual('@folio/stripes/components'),
-  KeyValue: jest.fn(({ label, children }) => (
-    <>
-      <span>{label}</span>
-      <span>{children}</span>
-    </>
-  )),
-  NoValue: jest.fn(({ ariaLabel }) => (<span>{ariaLabel}</span>)),
 }));
