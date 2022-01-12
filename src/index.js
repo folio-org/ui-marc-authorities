d57d5b69eb0a642f245e74c8fd2d65c13b7fce0f
Svitlana Korol <svitlana_korol@epam.com>
2022-01-12 16:57:14 +0200

    UIMARCAUTH-30:refactored code and added tests

--- src/index.js
+++ src/index.js
@@ -1,36 +1,23 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-import { useState } from 'react';
-import { FormattedMessage } from 'react-intl';
-
 import PropTypes from 'prop-types';
+
 import {
   Switch,
 } from 'react-router-dom';
 
 import {
-  AppContextMenu,
   Route,
 } from '@folio/stripes/core';
-
-import {
-  NavList,
-  NavListItem,
-  NavListSection,
-  CommandList,
-  HasCommand,
-  KeyboardShortcutsModal,
-  checkScope,
-} from '@folio/stripes/components';
-
-import commands from './commands';
 
 import {
   SearchRoute,
   AuthorityViewRoute,
   AuthorityQuickMarcEditRoute,
 } from './routes';
+
+import { MarcAuthoritiesAppContext } from './components';
 
 const propTypes = {
   match: PropTypes.object.isRequired,
@@ -44,53 +31,19 @@ const MarcAuthorities = ({
     path,
   },
 }) => {
-  const [isShortcutsModalOpen, setIsShortcutsModalOpen] = useState(false);
-
-  const shortcuts = [{
-    name: 'openShortcutModal',
-    handler: () => setIsShortcutsModalOpen(true),
-  }];
-
   return (
     <>
-      <CommandList commands={commands}>
-        <HasCommand
-          commands={shortcuts}
-          isWithinScope={checkScope}
-          scope={document.body}
+      <MarcAuthoritiesAppContext />
+      <Switch>
+        <Route path={`${path}/quick-marc`} component={AuthorityQuickMarcEditRoute} />
+        <Route
+          path={path}
+          component={SearchRoute}
         >
-          <AppContextMenu>
-            {(handleToggle) => (
-              <NavList>
-                <NavListSection>
-                  <NavListItem
-                    id="marc-authorities-app-item"
-                    onClick={() => setIsShortcutsModalOpen(handleToggle)}
-                  >
-                    <FormattedMessage id="ui-marc-authorities.appMenu.keyboardShortcuts" />
-                  </NavListItem>
-                </NavListSection>
-              </NavList>
-            )}
-          </AppContextMenu>
-          <Switch>
-            <Route path={`${path}/quick-marc`} component={AuthorityQuickMarcEditRoute} />
-            <Route
-              path={path}
-              component={SearchRoute}
-            >
-              <Route path={`${path}/authorities/:id`} component={AuthorityViewRoute} />
-            </Route>
-          </Switch>
-        </HasCommand>
-      </CommandList>
-      {isShortcutsModalOpen && (
-        <KeyboardShortcutsModal
-          onClose={() => setIsShortcutsModalOpen(false)}
-          allCommands={[...commands]}
-        />
-      )}
-    </ >
+          <Route path={`${path}/authorities/:id`} component={AuthorityViewRoute} />
+        </Route>
+      </Switch>
+    </>
   );
 };
 
