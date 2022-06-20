e9e24532ff04bbf5d062d9a72eab350d29353929
Mariia Sychova <mariia_sychova@epam.com>
2022-06-21 00:22:06 +0200

    UIMARCAUTH-152 Fix an error appears when opening already deleted MARC Authority record

--- src/queries/useMarcSource.js
+++ src/queries/useMarcSource.js
@@ -1,4 +1,13 @@
-import { useQuery } from 'react-query';
+import {
+  useQuery,
+  useQueryClient,
+} from 'react-query';
+import {
+  useLocation,
+  useHistory,
+} from 'react-router';
+import queryString from 'query-string';
+import pick from 'lodash/pick';
 
 import {
   useOkapiKy,
@@ -8,13 +17,32 @@ import {
 const MARC_SOURCE_API = id => `source-storage/records/${id}/formatted?idType=AUTHORITY`;
 
 export const useMarcSource = recordId => {
+  const location = useLocation();
+  const history = useHistory();
+  const queryClient = useQueryClient();
+
   const ky = useOkapiKy();
   const [namespace] = useNamespace();
 
   const { isFetching, data } = useQuery(
     [namespace, 'authority-source', recordId],
     async () => {
-      return ky.get(MARC_SOURCE_API(recordId)).json();
+      return ky.get(MARC_SOURCE_API(recordId)).json()
+        .catch(async err => {
+          const errorResponse = await err.response;
+
+          if (errorResponse.status === 404) {
+            const parsedSearchParams = queryString.parse(location.search);
+            const commonSearchParams = pick(parsedSearchParams, ['query', 'segment']);
+            const newSearchParamsString = queryString.stringify(commonSearchParams);
+
+            queryClient.invalidateQueries(namespace);
+            history.push({
+              pathname: '/marc-authorities',
+              search: newSearchParamsString,
+            });
+          }
+        });
     },
   );
 
