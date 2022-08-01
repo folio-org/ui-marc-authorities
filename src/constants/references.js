33cf2204769dc9425d147bcf01c98b2f1a17909c
Denys Bohdan <denys_bohdan@epam.com>
2022-08-01 15:18:55 +0300

    UIMARCAUTH-162 Browse authority | Remove checkbox 'Exclude see from also' from 'Reference' accordion. (#184)

--- src/constants/references.js
+++ src/constants/references.js
@@ -1,15 +1,25 @@
+import { navigationSegments } from './navigationSegments';
+
 export const REFERENCES_VALUES_MAP = {
   excludeSeeFrom: 'excludeSeeFrom',
   excludeSeeFromAlso: 'excludeSeeFromAlso',
 };
 
-export const REFERENCES_OPTIONS = [
-  {
-    labelId: `ui-marc-authorities.search.${REFERENCES_VALUES_MAP.excludeSeeFrom}`,
-    value: REFERENCES_VALUES_MAP.excludeSeeFrom,
-  },
-  {
-    labelId: `ui-marc-authorities.search.${REFERENCES_VALUES_MAP.excludeSeeFromAlso}`,
-    value: REFERENCES_VALUES_MAP.excludeSeeFromAlso,
-  },
-];
+export const REFERENCES_OPTIONS_BY_SEGMENT = {
+  [navigationSegments.search]: [
+    {
+      labelId: `ui-marc-authorities.search.${REFERENCES_VALUES_MAP.excludeSeeFrom}`,
+      value: REFERENCES_VALUES_MAP.excludeSeeFrom,
+    },
+    {
+      labelId: `ui-marc-authorities.search.${REFERENCES_VALUES_MAP.excludeSeeFromAlso}`,
+      value: REFERENCES_VALUES_MAP.excludeSeeFromAlso,
+    },
+  ],
+  [navigationSegments.browse]: [
+    {
+      labelId: `ui-marc-authorities.search.${REFERENCES_VALUES_MAP.excludeSeeFrom}`,
+      value: REFERENCES_VALUES_MAP.excludeSeeFrom,
+    },
+  ],
+};
