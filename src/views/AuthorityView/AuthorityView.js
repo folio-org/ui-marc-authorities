32b49f0e97a2135e1b30c8746b43b6b3aa8bc22d
Dmytro-Melnyshyn <Dmytro_Melnyshyn@epam.com>
2022-08-25 22:07:07 +0300

    move markHighlightedFields to stripes-authority-components

--- src/views/AuthorityView/AuthorityView.js
+++ src/views/AuthorityView/AuthorityView.js
@@ -14,8 +14,6 @@ import {
 } from 'react-intl';
 import queryString from 'query-string';
 import omit from 'lodash/omit';
-import cloneDeep from 'lodash/cloneDeep';
-import set from 'lodash/set';
 
 import {
   LoadingPane,
@@ -31,7 +29,10 @@ import {
   CalloutContext,
 } from '@folio/stripes/core';
 import MarcView from '@folio/quick-marc/src/QuickMarcView/QuickMarcView';
-import { SelectedAuthorityRecordContext } from '@folio/stripes-authority-components';
+import {
+  markHighlightedFields,
+  SelectedAuthorityRecordContext,
+} from '@folio/stripes-authority-components';
 
 import { KeyShortCutsWrapper } from '../../components';
 
@@ -141,45 +142,6 @@ const AuthorityView = ({
     return stripes.hasPerm('ui-marc-authorities.authority-record.delete');
   };
 
-  const markHighlightedFields = () => {
-    const highlightAuthRefFields = {
-      'Authorized': /1\d\d/,
-      'Reference': /4\d\d/,
-      'Auth/Ref': /5\d\d/,
-    };
-
-    const marcFields = marcSource.data.parsedRecord.content.fields.map(field => {
-      const tag = Object.keys(field)[0];
-
-      const isHighlightedTag = highlightAuthRefFields[authority.data.authRefType].test(tag);
-
-      if (!isHighlightedTag) {
-        return field;
-      }
-
-      const fieldContent = field[tag].subfields.reduce((contentArr, subfield) => {
-        const subfieldValue = Object.values(subfield)[0];
-
-        return [...contentArr, subfieldValue];
-      }, []).join(' ');
-
-      const isHeadingRefMatching = fieldContent === authority.data.headingRef;
-
-      return {
-        ...field,
-        [tag]: {
-          ...field[tag],
-          isHighlighted: isHeadingRefMatching && isHighlightedTag,
-        },
-      };
-    });
-    const marcSourceClone = cloneDeep(marcSource);
-
-    set(marcSourceClone, 'data.parsedRecord.content.fields', marcFields);
-
-    return marcSourceClone;
-  };
-
   const onConfirmDelete = () => {
     deleteItem(authority.data.id);
     setDeleteModalOpen(false);
@@ -208,7 +170,7 @@ const AuthorityView = ({
         marcTitle={intl.formatMessage({
           id: 'stripes-authority-components.marcHeading',
         })}
-        marc={markHighlightedFields().data}
+        marc={markHighlightedFields(marcSource, authority).data}
         onClose={onClose}
         lastMenu={
           <>
