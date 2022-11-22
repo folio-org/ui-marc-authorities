a4f33ebce93b404f7561e5e0a110fc2cb0fef3ca
Denys Bohdan <denys_bohdan@epam.com>
2022-11-22 17:23:21 +0200

    UIMARCAUTH-199 Loading Pane snapping out of resize cache. (#215)

--- src/views/AuthorityView/AuthorityView.js
+++ src/views/AuthorityView/AuthorityView.js
@@ -113,7 +113,7 @@ const AuthorityView = ({
   });
 
   if (marcSource.isLoading || authority.isLoading) {
-    return <LoadingPane />;
+    return <LoadingPane id="marc-view-pane" />;
   }
 
   if (!authority.data) {
