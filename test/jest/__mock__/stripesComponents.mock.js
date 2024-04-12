c88bf652ba88904adfcd67a52aa09fd4d6389251
Zak Burke <zburke@ebsco.com>
2024-04-12 12:34:54 -0400

    do not mock stripes-components/Icon to avoid breaking existing selectors

--- test/jest/__mock__/stripesComponents.mock.js
+++ test/jest/__mock__/stripesComponents.mock.js
@@ -1,5 +1,4 @@
 jest.mock('@folio/stripes/components', () => ({
   ...jest.requireActual('@folio/stripes/components'),
-  Icon: ({ children, icon }) => (children || <span>{icon}</span>),
   LoadingView: () => <div>Loading view</div>,
 }));
